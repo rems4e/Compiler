1f1eea6ae6246662fa2d853c072bd0a130de7477
Rémi <saurel@etud.insa-toulouse.fr>
2015-03-09 09:45:24 +0100

    Test

--- test.c
+++ test.c
@@ -1,4 +1,23 @@
-int main {
-	int a = 3 || 4;
+int main { 
+	int i, j, k, r, l, a;
+	i = 3;
+	a = 0;
+	j = 4;
+	k = 8;
+
+	if(i > 0) {
+		l = 4;
+	}
+	
+	while(a < 12) {
+		a = a + 1;
+	}
+	
+	printf(i);
+	r = (i + j) * (i + k / j);
+	printf(r);
+	
+	
+	
 	return 0;
 }
